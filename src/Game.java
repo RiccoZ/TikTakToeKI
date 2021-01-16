2ac751f5165d69a1c4afda29b1fa6d40fd14c245
RiccoZ <G6bIRsgCuS.1998.2811>
2021-01-16 05:35:18 +0100

    KI can place circle full KI reaction not ready

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Game {
 
     int turn = 1;
     GameFrame gf;
     int[][] field = {
-            {1,2,0},
+            {0,0,0},
             {0,0,0},
             {0,0,0}
     };
@@ -13,21 +16,25 @@ public class Game {
     }
 
     public void click(int x, int y) {
+        x--;
+        y--;
         if(turn == 1) {
-            if((x >= 1) && (x <= 3) && (y >= 1) && (y <= 3)) { //click must be in gamefield
-                if(field[x-1][y-1] == 0) {
-                    x = x * 100; //middle of square
-                    y = 50 + y * 100; //middle of square
-                    gf.add(x, y);
+            if((x >= 0) && (x <= 2) && (y >= 0) && (y <= 2)) { //click must be in gamefield
+                if(field[x][y] == 0) {
                     field[x][y] = 1; //add mark to array
+                    int xnew = (x+1) * 100; //middle of square
+                    int ynew = 50 + (y+1) * 100; //middle of square
+                    gf.addcross(xnew, ynew);
                 }
             }
             turn = 2;
+
+            checkwin(x, y);
+            react();
         }
-        checkwin(1, x, y);
     }
 
-    private boolean checkwin(int player, int x, int y) { //check if player {player} won
+    private boolean checkwin(int x, int y) { //check if player {player} won
         int sum = 0;
 
         for(int i : field[y]) {
@@ -35,16 +42,94 @@ public class Game {
             System.out.println(sum);
         }
 
+        if(sum == 3) {
+            System.out.println("win");
+            turn = 0;
+            return true;
+        }
+
         sum = 0;
+
         for(int i : field[x]) {
             sum += i;
             System.out.println(sum);
         }
+
+        if(sum == 3) {
+            System.out.println("win");
+            turn = 0;
+            return true;
+        }
+
         sum = 0;
+
         if(y == x) {
             sum = field[0][0] + field[1][1] + field[2][2];
         }
 
-        return true;
+        if(sum == 3) {
+            System.out.println("win");
+            turn = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    private ArrayList<Integer> getnums() {
+        ArrayList<Integer> listofnums = new ArrayList<Integer>();
+
+        for (int x = 0; x < 3; x++) {
+            int sum = 0;
+            for (int y = 0; y < 3; y++) {
+                sum += field[x][y];
+            }
+            listofnums.add(sum);
+        }
+
+        for (int y = 0; y < 3; y ++) {
+            int sum = 0;
+            for (int x = 0; x < 3; x++) {
+                sum += field[x][y];
+            }
+            listofnums.add(sum);
+        }
+
+        listofnums.add(field[0][0] + field[1][1] + field[2][2]);
+        listofnums.add(field[0][2] + field[1][1] + field[2][0]);
+
+        return listofnums;
+    }
+
+    private void react() {
+        System.out.println(getnums());
+
+        ArrayList<Integer> nums = getnums();
+
+        int pos = 0;
+
+        if (nums.contains(-2)) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums.get(i) == -2) {
+                    pos = i;
+                }
+            }
+        }
+
+        int x = pos % 3;
+
+        if ((pos / 3) == 1) {
+            for (int y = 0; y < 3; y++) {
+                if (field[x][y] == 0) {
+                    field[x][y] = -1; //add mark to array
+                    int xnew = (x+1) * 100; //middle of square
+                    int ynew = 50 + (y+1) * 100; //middle of square
+                    gf.addcircle(xnew, ynew);
+                    turn = 1;
+                    checkwin(x,y);
+                    return;
+                }
+            }
+        }
     }
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.geom.Line2D;
+import java.awt.geom.*;
 import java.util.ArrayList;
+
+import static java.lang.System.exit;
 
 public class GameFrame extends JFrame {
 
     Game g = new Game(this);
 
     private ArrayList<Line2D> linelist = new ArrayList<>();
+    private ArrayList<Ellipse2D> ellipselist = new ArrayList<>();
 
     public GameFrame() {
         initialize();
@@ -18,14 +23,26 @@ public class GameFrame extends JFrame {
     private void initialize(){
         setSize(800, 700);
         setTitle("Tik Tak Toe");
+        setLayout(null);
         getContentPane().setBackground(Color.lightGray);
 
+        JButton btnclose = new JButton("Beenden");
+        btnclose.setBounds(650,600,120,40);
+        btnclose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exit(0);
+            }
+        });
+
+
+        add(btnclose);
         setVisible(true);
 
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                int  x= e.getX();
+                int x = e.getX();
                 int y = e.getY();
 
                 x = (x + 50) / 100; //check in which square the click is
@@ -56,9 +73,14 @@ public class GameFrame extends JFrame {
         });
     }
 
-    public void add(int x, int y) {
+    public void addcross(int x, int y) {
         linelist.add(new Line2D.Float(x - 30, y - 30, x + 30, y + 30));
         linelist.add(new Line2D.Float(x + 30, y - 30, x - 30, y + 30));
+        repaint();
+    }
+
+    public void addcircle(int x, int y) {
+        ellipselist.add(new Ellipse2D.Double(x - 35 , y - 35 , 70, 70));
         repaint();
     }
 
@@ -71,9 +93,12 @@ public class GameFrame extends JFrame {
         linelist.add(new Line2D.Float(50,200,350,200));
         linelist.add(new Line2D.Float(50,300,350,300));
 
-
         for (Line2D line: linelist) {
             g2.draw(line);
+        }
+
+        for (Ellipse2D ellipse: ellipselist) {
+            g2.draw(ellipse);
         }
     }
 }
